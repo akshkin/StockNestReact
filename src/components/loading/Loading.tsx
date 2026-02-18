60dd534a8c728f9b5de5f032b0da8dd07d47286e
Akshaya <akshaya.kini12@gmail.com>
2026-02-18 14:19:50 +0100

    Create a reusable loading component

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { VscLoading } from "react-icons/vsc";
+import styles from "./loading.module.scss";
+
+function Loading() {
+	return (
+		<div className={styles.loadingContainer}>
+			<VscLoading className="loading" />
+		</div>
+	);
+}
+
+export default Loading;
