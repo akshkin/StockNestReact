f06e5d14a7de37e0fff1db46de82c1ca6fd8bd91
Akshaya <akshaya.kini12@gmail.com>
2026-02-18 15:40:30 +0100

    Style the modal and add the form for creating a group in it

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./modal.module.scss";
+import { IoCloseOutline } from "react-icons/io5";
 
 type ModalProps = {
 	title: string;
@@ -11,9 +12,13 @@ function Modal({ title, children, closeModal }: ModalProps) {
 	return (
 		<div className={styles.backdrop}>
 			<div className={styles.modal}>
-				<h2>{title}</h2>
-				<button onClick={closeModal}>X</button>
-				{children}
+				<header className={styles.modalHeader}>
+					<h3>{title}</h3>
+					<button className={styles.closeBtn} onClick={closeModal}>
+						<IoCloseOutline />
+					</button>
+				</header>
+				<div>{children}</div>
 			</div>
 		</div>
 	);
