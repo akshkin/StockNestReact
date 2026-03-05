c611c090b712bce647329c0424f52006973485b9
Akshaya <akshaya.kini12@gmail.com>
2026-03-05 20:55:31 +0100

    Add environment variable for api url and use in apiSlice

--- src/api/apiSlice.ts
+++ src/api/apiSlice.ts
@@ -7,8 +7,10 @@ import type {
 } from "@reduxjs/toolkit/query";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const baseUrl = import.meta.env.VITE_BASE_URL;
+
 const baseQuery = fetchBaseQuery({
-	baseUrl: "https://localhost:7036/api",
+	baseUrl: baseUrl,
 	credentials: "include",
 });
 
