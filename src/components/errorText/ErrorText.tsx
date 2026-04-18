4a008fa571110c1352b7d7a8cfbbd31c5ef034ba
Akshaya <akshaya.kini12@gmail.com>
2026-04-18 20:43:39 +0200

    Modify error text file

--- src/components/errorText/ErrorText.tsx
+++ src/components/errorText/ErrorText.tsx
@@ -1,8 +1,7 @@
 import styles from "./errorText.module.scss";
 
 function ErrorText({ error }: { error?: string }) {
-	return <div className={styles.error}>
-		{error}
+	return <div className={styles.error}>{error}
 		</div>;
 }
 
