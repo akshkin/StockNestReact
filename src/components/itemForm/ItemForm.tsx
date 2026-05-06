1ee5f6b28b4705a7bef578cb9159a9aaab34dc26
Akshaya <akshaya.kini12@gmail.com>
2026-05-06 15:27:56 +0200

    Add functionality to update item quantity when an item with the same name already exists in a category

--- src/components/itemForm/ItemForm.tsx
+++ src/components/itemForm/ItemForm.tsx
@@ -3,114 +3,167 @@ import InputField from "../inputField/InputField";
 import { useZodForm } from "../../hooks/useZodForm";
 import type z from "zod";
 import {
-	useCreateItemMutation,
-	useUpdateItemMutation,
+  useCreateItemMutation,
+  useUpdateItemMutation,
+  type Item,
 } from "../../api/itemsApi";
 import ErrorText from "../errorText/ErrorText";
 import { itemSchema } from "../../schemas";
 import { toast } from "react-toastify";
-import { extractErrorMessage } from "../../helpers/utils";
+import {
+  checkDuplicateItemError,
+  extractErrorMessage,
+} from "../../helpers/utils";
 
 type itemSchema = z.infer<typeof itemSchema>;
 
 type ItemFormProps = {
-	mode: "Add" | "Edit"; //edit, add
-	groupId: number;
-	categoryId: number;
-	itemId?: number;
-	closeModal: () => void;
-	initialValues?: itemSchema;
+  mode: "Add" | "Edit"; //edit, add
+  groupId: number;
+  categoryId: number;
+  itemId?: number;
+  closeModal: () => void;
+  initialValues?: itemSchema;
 };
 
 const defaultValues = {
-	name: "",
-	quantity: 0,
+  name: "",
+  quantity: 0,
 };
 
 function ItemForm({
-	mode,
-	groupId,
-	categoryId,
-	itemId,
-	closeModal,
-	initialValues,
+  mode,
+  groupId,
+  categoryId,
+  itemId,
+  closeModal,
+  initialValues,
 }: ItemFormProps) {
-	const [formError, setFormError] = useState<string | null>(null);
-	const { data, update, errors, isValid } = useZodForm<itemSchema>(
-		itemSchema,
-		initialValues
-			? initialValues
-			: {
-					...defaultValues,
-				},
-		() => setFormError(null),
-	);
+  const [formError, setFormError] = useState<string | null>(null);
+  const { data, update, errors, isValid } = useZodForm<itemSchema>(
+    itemSchema,
+    initialValues
+      ? initialValues
+      : {
+          ...defaultValues,
+        },
+    () => setFormError(null),
+  );
 
-	const [createItem] = useCreateItemMutation();
-	const [updateItem] = useUpdateItemMutation();
+  const [createItem] = useCreateItemMutation();
+  const [updateItem] = useUpdateItemMutation();
+  const [showUpdateQuantityOptions, setShowUpdateQuantityOptions] =
+    useState(false);
+  const [existingItemData, setExistingItemData] = useState<Item | null>(null);
 
-	const isEditing = mode === "Edit";
+  const isEditing = mode === "Edit";
 
-	async function handleSubmit(e: React.FormEvent) {
-		e.preventDefault();
-		let res;
-		if (isEditing) {
-			res = await updateItem({ groupId, categoryId, itemId, formData: data });
-		} else {
-			res = await createItem({
-				groupId,
-				categoryId,
-				formData: data,
-			});
-		}
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    let res;
+    if (isEditing) {
+      res = await updateItem({ groupId, categoryId, itemId, formData: data });
+    } else {
+      res = await createItem({
+        groupId,
+        categoryId,
+        formData: data,
+      });
+    }
 
-		if (!("error" in res)) {
-			if (isEditing) {
-				toast.success("Successfully updated item!");
-			} else {
-				toast.success("Successfuly created item");
-			}
-			return closeModal();
-		} else {
-			if ("error" in res) {
-				const err = extractErrorMessage(res?.error);
-				setFormError(err);
-			}
-		}
-	}
+    if (!("error" in res)) {
+      if (isEditing) {
+        toast.success("Successfully updated item!");
+      } else {
+        toast.success("Successfuly created item");
+      }
+      return closeModal();
+    } else {
+      if ("error" in res) {
+        const err = extractErrorMessage(res?.error);
+        setFormError(err);
 
-	return (
-		<form className="group-category-item-form">
-			<InputField
-				label="Item name"
-				name="name"
-				value={data.name}
-				placeholder="Enter name of item e.g. Rice"
-				onChange={(e) => update("name", e.target.value)}
-				error={errors.name}
-			/>
-			<InputField
-				label="Quantity"
-				name="quantity"
-				value={data.quantity.toString()}
-				placeholder="Enter quantity of item"
-				onChange={(e) => update("quantity", e.target.value)}
-				error={errors.quantity}
-				inputMode="numeric"
-				pattern="[0-9]"
-				onBeforeInput={(e) => {
-					// disable typing non-numeric input
-					if (!/^\d$/.test(e.data)) {
-						e.preventDefault();
-					}
-				}}
-			/>
-			{formError && <ErrorText error={formError} />}
-			<button disabled={!isValid} onClick={handleSubmit}>
-				{isEditing ? "Update" : "Add"}
-			</button>
-		</form>
-	);
+        const existingItem = checkDuplicateItemError(res.error);
+        if (existingItem) {
+          // allow users to update quantity of existing item if they try to add an item with the same name in the same category
+          setFormError(
+            `${existingItem.name} already exists in this category. Would you like to update the quantity instead?`,
+          );
+          setShowUpdateQuantityOptions(true);
+          setExistingItemData(existingItem);
+          return;
+        }
+      }
+    }
+  }
+
+  async function updateFromCreate() {
+    setShowUpdateQuantityOptions(false);
+
+    const existingItem = existingItemData;
+    if (!existingItem) return;
+
+    const res = await updateItem({
+      groupId,
+      categoryId,
+      itemId: existingItem.itemId,
+      formData: data,
+    });
+
+    if (!("error" in res)) {
+      toast.success("Successfully updated item!");
+      return closeModal();
+    }
+  }
+
+  return (
+    <form className="group-category-item-form">
+      <InputField
+        label="Item name"
+        name="name"
+        value={data.name}
+        placeholder="Enter name of item e.g. Rice"
+        onChange={(e) => update("name", e.target.value)}
+        error={errors.name}
+      />
+      <InputField
+        label="Quantity"
+        name="quantity"
+        value={data.quantity.toString()}
+        placeholder="Enter quantity of item"
+        onChange={(e) => update("quantity", e.target.value)}
+        error={errors.quantity}
+        inputMode="numeric"
+        pattern="[0-9]"
+        onBeforeInput={(e) => {
+          // disable typing non-numeric input
+          if (!/^\d$/.test(e.data)) {
+            e.preventDefault();
+          }
+        }}
+      />
+      {formError && <ErrorText error={formError} />}
+      {!showUpdateQuantityOptions && (
+        <button disabled={!isValid} onClick={handleSubmit} className="button">
+          {isEditing ? "Update" : "Add"}
+        </button>
+      )}
+      {showUpdateQuantityOptions && (
+        <>
+          <p>Would you like to update the existing item instead?</p>
+          <button
+            onClick={() => {
+              setShowUpdateQuantityOptions(false);
+              closeModal();
+            }}
+          >
+            No
+          </button>
+          <button onClick={updateFromCreate}>Yes</button>
+        </>
+      )}
+    </form>
+  );
 }
 
 export default ItemForm;
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,77 +1,113 @@
 import { ZodError } from "zod";
 import type { NotificationItemType } from "../api/notificationsApi";
 import type { NavigateFunction } from "react-router-dom";
+import type { Item } from "../api/itemsApi";
 
 export function zodErrorsToObject<T extends object>(error: ZodError<T>) {
-	const errors: Partial<Record<keyof T, string>> = {};
+  const errors: Partial<Record<keyof T, string>> = {};
 
-	for (const issue of error.issues) {
-		const key = issue.path[0] as keyof T;
-		errors[key] = issue.message;
-	}
+  for (const issue of error.issues) {
+    const key = issue.path[0] as keyof T;
+    errors[key] = issue.message;
+  }
 
-	return errors;
+  return errors;
 }
 
 export function navigateFromNotification(
-	n: NotificationItemType,
-	navigate: NavigateFunction,
+  n: NotificationItemType,
+  navigate: NavigateFunction,
 ) {
-	const routes = {
-		ItemCreated: () =>
-			navigate(`/groups/${n.groupId}/category/${n.categoryId}`, {
-				state: { itemId: n.itemId },
-			}),
+  const routes = {
+    ItemCreated: () =>
+      navigate(`/groups/${n.groupId}/category/${n.categoryId}`, {
+        state: { itemId: n.itemId },
+      }),
 
-		ItemUpdated: () =>
-			navigate(`/groups/${n.groupId}/category/${n.categoryId}`, {
-				state: { itemId: n.itemId },
-			}),
+    ItemUpdated: () =>
+      navigate(`/groups/${n.groupId}/category/${n.categoryId}`, {
+        state: { itemId: n.itemId },
+      }),
 
-		ItemDeleted: () =>
-			navigate(`/groups/${n.groupId}/category/${n.categoryId}`),
+    ItemDeleted: () =>
+      navigate(`/groups/${n.groupId}/category/${n.categoryId}`),
 
-		CategoryCreated: () =>
-			navigate(`/groups/${n.groupId}`, {
-				state: { categoryId: n.categoryId },
-			}),
+    CategoryCreated: () =>
+      navigate(`/groups/${n.groupId}`, {
+        state: { categoryId: n.categoryId },
+      }),
 
-		CategoryUpdated: () =>
-			navigate(`/groups/${n.groupId}`, {
-				state: { categoryId: n.categoryId },
-			}),
+    CategoryUpdated: () =>
+      navigate(`/groups/${n.groupId}`, {
+        state: { categoryId: n.categoryId },
+      }),
 
-		CategoryDeleted: () => navigate(`/groups/${n.groupId}`),
+    CategoryDeleted: () => navigate(`/groups/${n.groupId}`),
 
-		UserJoinedGroup: () => navigate(`/groups/${n.groupId}`),
+    UserJoinedGroup: () => navigate(`/groups/${n.groupId}`),
 
-		UserRemovedFromGroup: () => navigate(`/groups/${n.groupId}`),
+    UserRemovedFromGroup: () => navigate(`/groups/${n.groupId}`),
 
-		GroupUpdated: () =>
-			navigate(`/groups`, {
-				state: { groupId: n.groupId },
-			}),
-		GroupDeleted: () => navigate(`/groups`),
-	};
+    GroupUpdated: () =>
+      navigate(`/groups`, {
+        state: { groupId: n.groupId },
+      }),
+    GroupDeleted: () => navigate(`/groups`),
+  };
 
-	routes[n.type]?.();
+  routes[n.type]?.();
 }
 
 export function getPermissions(role?: string) {
-	const ownerPermission = role === "Owner" || role === "Co-Owner";
-	const canCreateEdit = role !== "Viewer";
+  const ownerPermission = role === "Owner" || role === "Co-Owner";
+  const canCreateEdit = role !== "Viewer";
 
-	return { ownerPermission, canCreateEdit };
+  return { ownerPermission, canCreateEdit };
 }
 
 export function extractErrorMessage(error: unknown): string {
-	if (typeof error === "object" && error !== null && "data" in error) {
-		const err = error as { data?: unknown };
+  if (typeof error === "object" && error !== null && "data" in error) {
+    const err = error as { data?: unknown };
 
-		if (err.data && typeof err.data === "object" && "message" in err.data) {
-			return err.data.message as string;
-		}
-	}
+    if (err.data && typeof err.data === "object" && "message" in err.data) {
+      return err.data.message as string;
+    }
+  }
 
-	return "An unexpected error occurred";
+  return "An unexpected error occurred";
 }
+
+export function checkDuplicateItemError(error: unknown): Item | null {
+  if (!error) return null;
+  if (typeof error !== "object" || error === null) {
+    return null;
+  } else {
+    if ("status" in error && error.status !== 409) return null;
+
+    if (!("data" in error && "data" in error.data)) {
+      const item = error?.data?.data?.existingItem;
+
+      if (!item) return null;
+
+      return item;
+    }
+
+    return null;
+  }
+}
+
+// type ApiError<T = unknown> = {
+// 	status: number;
+// 	message: string;
+// 	data?: T;
+// };
+
+// function normalizeError<T = unknown>(error: unknown): ApiError<T> {
+// 	if (typeof error === "object" && error !== null) {
+// 		return {
+// 			status: error?.status,
+// 			message: error?.data?.message,
+// 			data: error?.data?.data, // <-- unwrap once here
+// 		};
+// 	}
+// }
