b563ef89ef66e93e9be3cfcc734a20c3d43c0376
Akshaya <akshaya.kini12@gmail.com>
2026-05-13 15:57:23 +0200

    Create an offlineText component to show message to the user when offline

---
+++ src/components/offlineText/OfflineText.tsx
@@ -0,0 +1,13 @@
+function OfflineText() {
+  return (
+    <div style={{ padding: 24, textAlign: "center" }}>
+      <h2>You appear to be offline</h2>
+      <p>
+        This app needs internet connectivity for data sync. Being offline means
+        some features may not be available.
+      </p>
+    </div>
+  );
+}
+
+export default OfflineText;
